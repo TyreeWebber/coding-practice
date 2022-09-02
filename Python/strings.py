0ffbfab5bf6916ef905431c1b3cde1e97f8de2c8
Tyree Webber <webber.tyree@gmail.com>
2022-09-02 11:08:20 -0600

    initial commit of coding practice repo, contains javascript and python, will eventually work towards more difficult concepts and functions

---
+++ Python/strings.py
@@ -0,0 +1,11 @@
+# Strings
+# A string can contain numbers, letters and symbols
+# Declaration examples
+
+# Single line string
+string = "This is an example of a string"
+
+# Multi line string
+a_world_of_dew = """A world of dew,
+And within every dewdrop
+A world of struggle."""
