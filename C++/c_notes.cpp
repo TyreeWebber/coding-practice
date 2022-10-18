3f7ab4c1e4b6ebb7aa7e1da77c1dc120495bedcb
Tyree Webber <webber.tyree@gmail.com>
2022-10-18 12:47:52 -0600

    add exceptions to c++ notes

--- C++/c_notes.cpp
+++ C++/c_notes.cpp
@@ -3,3 +3,13 @@
 //C++ allows us to define our own abstractions but it also comes with built-in abstractions.
 //The range based for-loop makes it possible to iterate over a range of values (an array in JS).
 
+// Exceptions
+// A C++ exception is a response to an exceptional circumstance that arises while a program is running, such as an attempt to divide by zero.
+// Exceptions provide a way to transfer control from one part of a program to another. C++ exception handling is built upon three keywords: try, catch, and throw.
+// Throw
+// A program throws an exception when a problem shows up. This is done using a throw keyword.
+// Catch
+// A program catches an exception with an exception handler at the place in a program where you want to handle the problem. The catch keyword indicates the catching of an exception.
+// Try
+// A try block identifies a block of code for which particular exceptions will be activated. It's followed by one or more catch blocks.
+
